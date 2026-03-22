9e43e7f0c0911e5cfd706eaa3e02486bbbf1fb6a
AjMing <62439174+AjMing@users.noreply.github.com>
2026-03-22 23:03:10 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+#include "student.h"
+#define N 3
+int main(int argc, char* argv[]) {
+  student s1,s2;
+
+ s1.set_name("Amy"); s1.set_age(20); s2.set_name("James"); s2.set_age(17); s1.get_age()>s2.get_age() ? s1.print_name(); cout<< "is older":s2.print_name(); cout <<"is older";
+
+
+
+  student a[N];
+  // Set age and set name for the first two ppl
+  int i,age;
+  string n;
+   // 1. Ask for N names and N age using cin
+ 
+  
+ 
+  //2. Print name and age of all N ppl
+  
+  
+  
+  //3. Print name and age of 
+  //the youngest person
+  //Find what the age of the youngest person
+ 
+  
+  // Print all info for the yougest person
+ 
+  
+  
+  //4. Change input from cin to argv
+
+  
+ return 0;
+
+    
+  }
+  
+  
+
+
---
+++ student.h
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class student{
+private:
+
+  string name;
+
+public:
+  
+   void set_name(string="Yme");
+    void print_name();
+
+};
